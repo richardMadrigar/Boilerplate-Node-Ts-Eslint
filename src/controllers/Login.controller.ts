c24835ffb015ce9fce2febe9ec930110ff34b1be
Richard madrigar <richard_madrigar99@hotmail.com>
2022-01-12 17:10:43 -0300

    add dotenv, att pg, add Readme

--- src/controllers/Login.controller.ts
+++ src/controllers/Login.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 export const login = async (req: Request, res: Response) => {
   try {
-    return res.status(200).json('Login OK');
+    return res.status(200).json('Hello World');
   } catch (error) {
     return res.status(500).json('Internal Server error');
   }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 // importaçoes de rotas
 import indexRoutes from './routes/Login.routers';
 
 const app = express();
 
+const PORT = process.env.PORT_SERVER || 3001;
+
 app.use(express.json()); // aceitar requisiçoes do tipo json
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev')); // log no terminal
 app.use(cors()); // aceitar requisiçoes de outras rota
+dotenv.config();
 
 // executando rotas
 app.use(indexRoutes);
 
-app.listen(3001, () => {
-  console.log('Servidor rodando na porta', 3001);
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta', PORT);
 });
